80ff6553f416a49ef6eea7658c8b1bdfd0e08a81
george <ge77af@gmail.com>
2015-04-21 21:00:47 +0300

    created Coords and Ship for GameData

---
+++ NavyBattles-CSharp/New Folder/Coords.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: delta
+ * Date: 21/4/2015
+ * Time: 8:05 μμ
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NavyBattles_CSharp.New_Folder
+{
+	/// <summary>
+	/// Description of Coords.
+	/// </summary>
+	public class Coords
+	{
+		private int myX,myY;
+		public Coords(int x,int y)
+		{
+			myX=x;
+			myY=y;
+		}
+		public int X
+		{
+			get{ return myX;}
+			set{ myX=value;}
+		}
+		public int Y
+		{
+			get{ return myY;}
+			set{ myY=value;}
+		}
+		
+		
+	}
+	public enum Direction
+	{
+		HORIZONTAL,VERTICAL
+	}
+}
