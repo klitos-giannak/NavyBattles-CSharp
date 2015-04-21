7b5cb1f66784c6ae7dee6573f2f48c75a2ca3b46
george <ge77af@gmail.com>
2015-04-21 19:55:24 +0300

    initial GameData class

---
+++ NavyBattles-CSharp/New Folder/GameData.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: delta
+ * Date: 21/4/2015
+ * Time: 6:33 μμ
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NavyBattles_CSharp.New_Folder
+{
+	/// <summary>
+	/// Description of GameData.
+	/// </summary>
+	public class GameData
+	{
+		private const int GRIDSIZE=10;
+		
+		private BoxState[,] myGrid=new BoxState[GRIDSIZE,GRIDSIZE];
+		private BoxState[,] enemyGrid=new BoxState[GRIDSIZE,GRIDSIZE];
+			
+		public GameData()
+		{
+			clearGrids();
+		}
+		
+		public BoxState getMyBoxState(int x,int y)
+		{
+			return myGrid[x,y];
+		}
+		
+		public BoxState getEnemyBoxState(int x,int y)
+		{
+			return enemyGrid[x,y];
+		}
+		
+		public void setMyBoxState(int x,int y,BoxState state)
+		{
+			myGrid[x,y]=state; 
+		}
+		
+		public void setEnemyBoxState(int x,int y,BoxState state)
+		{
+			enemyGrid[x,y]=state; 
+		}
+		
+		private void clearGrids()
+		{
+			for (int i=0; i<myGrid.GetLength(0); i++)
+			{
+				for (int j=0; i<myGrid.GetLength(1); j++)
+				{
+					myGrid[i,j]= BoxState.CLEAR;
+					enemyGrid[i,j]= BoxState.CLEAR;
+				}
+			}
+		}	
+		
+		public enum BoxState
+		{
+			CLEAR,MISSED,BOMBER
+		}
+	}
+	 
+	
+}
